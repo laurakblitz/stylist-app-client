f6fb6a1ef47cb134853f5f491a724ddc590ec89c
Laura Blitz <laura.k.blitz@gmail.com>
2021-01-12 11:59:57 -0500

    Changes to Table

--- src/components/Home/Closet/ClosetTable.tsx
+++ src/components/Home/Closet/ClosetTable.tsx
@@ -5,9 +5,11 @@ import {
     CardContent,
     Table,
 } from '@material-ui/core';
+import { render } from '@testing-library/react';
 
 type Props = {
-    myCloset: any,
+    // myCloset: any,
+    myCloset: Array<{ id: number, image: string, category: number }>
     editUpdateCloset: (closet: any) => void,
     updateOn: () => void,
     fetchClosetPosts: () => void,
@@ -28,28 +30,48 @@ export default class ClosetTable extends React.Component<Props> {
         }).then(() => this.props.fetchClosetPosts())
     }
 
-    postMapper = () => {
-        let closets = this.props.myCloset;
-
-        return closets.map((closet: any, index: number) => {
-            return (
-                <Card key={index}>
-                    <CardContent><img src={closet.image} width="25%" height="25%" /></CardContent>
-                    <CardContent>{closet.category}</CardContent>
-                    <CardContent>
-                        <Button variant="outlined" onClick={() => { this.props.editUpdateCloset(closet); this.props.updateOn() }}>Edit</Button>
-                        <Button variant="outlined" onClick={() => { this.deletePost(closet) }}>Clear</Button>
-                    </CardContent>
-                </Card>
-            )
-        })
-    }
-
     render() {
         return (
-            <Table>
-                {this.postMapper()}
-            </Table>
+            <div>
+                {this.props.myCloset
+                    ? this.props.myCloset.map((myCloset) => (
+                        <Card key={myCloset.id}>
+                            <CardContent><img src={myCloset.image} width="25%" height="25%" /></CardContent>
+                            <CardContent>{myCloset.category}</CardContent>
+                            <CardContent>
+                                <Button variant="outlined" onClick={() => { this.props.editUpdateCloset(myCloset); this.props.updateOn() }}>Edit</Button>
+                                <Button variant="outlined" onClick={() => { this.deletePost(myCloset) }}>Clear</Button>
+                            </CardContent>
+                        </Card>
+                    ))
+                    : undefined}
+            </div>
         )
     }
 }
+
+    // postMapper = () => {
+    //     let closets = this.props.myCloset;
+
+    //     return closets.map((closet: any, index: number) => {
+    //         return (
+    //             <Card key={index}>
+    //                 <CardContent><img src={closet.image} width="25%" height="25%" /></CardContent>
+    //                 <CardContent>{closet.category}</CardContent>
+    //                 <CardContent>
+    //                     <Button variant="outlined" onClick={() => { this.props.editUpdateCloset(closet); this.props.updateOn() }}>Edit</Button>
+    //                     <Button variant="outlined" onClick={() => { this.deletePost(closet) }}>Clear</Button>
+    //                 </CardContent>
+    //             </Card>
+    //         )
+    //     })
+    // }
+
+    // render() {
+    //     return (
+    //         <Table>
+    //             {this.postMapper()}
+    //         </Table>
+    //     )
+    // }
+
--- src/components/Home/Wishlist/WishlistTable.tsx
+++ src/components/Home/Wishlist/WishlistTable.tsx
@@ -7,7 +7,7 @@ import {
 } from '@material-ui/core';
 
 type Props = {
-    myWishlist: any,
+    myWishlist: Array<{ id: number, image: string, comment: string }>,
     editUpdateWishlist: (wishlist: any) => void,
     updateOn: () => void,
     fetchWishlistPosts: () => void,
@@ -28,28 +28,48 @@ export default class WishlistTable extends React.Component<Props> {
         }).then(() => this.props.fetchWishlistPosts())
     }
 
-    postMapper = () => {
-        let wishlists = this.props.myWishlist;
-
-        return wishlists.map((wishlist: any, index: number) => {
-            return (
-                <Card key={index}>
-                    <CardContent><img src={wishlist.image} width="25%" height="25%" /></CardContent>
-                    <CardContent>{wishlist.comment}</CardContent>
-                    <CardContent>
-                        <Button variant="outlined" onClick={() => { this.props.editUpdateWishlist(wishlist); this.props.updateOn() }}>Edit</Button>
-                        <Button variant="outlined" onClick={() => { this.deletePost(wishlist) }}>Clear</Button>
-                    </CardContent>
-                </Card>
-            )
-        })
-    }
-
     render() {
         return (
-            <Table>
-                {this.postMapper()}
-            </Table>
+            <div>
+                {this.props.myWishlist
+                    ? this.props.myWishlist.map((myWishlist) => (
+                        <Card key={myWishlist.id}>
+                            <CardContent><img src={myWishlist.image} width="25%" height="25%" /></CardContent>
+                            <CardContent>{myWishlist.comment}</CardContent>
+                            <CardContent>
+                                <Button variant="outlined" onClick={() => { this.props.editUpdateWishlist(myWishlist); this.props.updateOn() }}>Edit</Button>
+                                <Button variant="outlined" onClick={() => { this.deletePost(myWishlist) }}>Clear</Button>
+                            </CardContent>
+                        </Card>
+                    ))
+                    : undefined}
+            </div>
         )
     }
 }
+
+//     postMapper = () => {
+//         let wishlists = this.props.myWishlist;
+
+//         return wishlists.map((wishlist: any, index: number) => {
+//             return (
+//                 <Card key={index}>
+//                     <CardContent><img src={wishlist.image} width="25%" height="25%" /></CardContent>
+//                     <CardContent>{wishlist.comment}</CardContent>
+//                     <CardContent>
+//                         <Button variant="outlined" onClick={() => { this.props.editUpdateWishlist(wishlist); this.props.updateOn() }}>Edit</Button>
+//                         <Button variant="outlined" onClick={() => { this.deletePost(wishlist) }}>Clear</Button>
+//                     </CardContent>
+//                 </Card>
+//             )
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <Table>
+//                 {this.postMapper()}
+//             </Table>
+//         )
+//     }
+// }
